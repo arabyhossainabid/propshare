c2845b965fbd19eb5369c3aa5d3a2d48cadabfa6
arabyhossainabid <arabyhossainabid@gmail.com>
2026-03-25 01:09:00 +0600

    feat(core): implement Deep Space design system and global dark theme

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['images.unsplash.com', 'avatar.iran.liara.run', 'images.pexels.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatar.iran.liara.run',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
